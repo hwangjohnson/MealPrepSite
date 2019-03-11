5d30b84b1426abbc931f127bcc27cd0222b0528a
hwangjohnson <johnson_hwang@outlook.com>
2019-03-11 13:13:46 +0700

    MyfirstApp

---
+++ src/firebase/index.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyDlU-7heKB81r9pUpCKpTiwj6taoenIeH4",
+    authDomain: "meal-prep-72554.firebaseapp.com",
+    databaseURL: "https://meal-prep-72554.firebaseio.com",
+    projectId: "meal-prep-72554",
+    storageBucket: "",
+    messagingSenderId: "953963384048"
+};
+firebase.initializeApp(config);
